33e68b40e18a5c423433e22eaf1561caa3c36549
Tejas Khera <tejaskhera191812@gmail.com>
2025-02-22 23:17:26 +0530

    Added error handling for task controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,51 +1,37 @@
 const taskModel = require("../models/taskModel");
-exports.getAllTasks = async (req, res) => {
-  try {
-    const allTasks = await taskModel.find({});
-    res.status(200).json({ status: "Success", data: { allTasks } });
-  } catch (err) {
-    res
-      .status(400)
-      .json({ status: "Failed", message: "Unable to get all tasks!" });
+const catchAsync = require("../utils/catchAsync");
+exports.getAllTasks = catchAsync(async (req, res) => {
+  const allTasks = await taskModel.find({});
+  res.status(200).json({ status: "Success", data: { allTasks } });
+  res
+    .status(400)
+    .json({ status: "Failed", message: "Unable to get all tasks!" });
+});
+exports.createTask = catchAsync(async (req, res) => {
+  if (req.body) {
+    const newTask = await taskModel.create(req.body);
+    res.status(201).json({ status: "Success", data: { newTask } });
   }
-};
-exports.createTask = async (req, res) => {
-  try {
-    if (req.body) {
-      const newTask = await taskModel.create(req.body);
-      res.status(201).json({ status: "Success", data: { newTask } });
-    }
-  } catch (err) {
-    res.status(400).json({ status: "Failed", message: err });
-  }
-};
-exports.getTask = async (req, res) => {
-  try {
-    const user = await taskModel.findById(req.params.id);
-    res.status(200).json({ status: "Success", data: { user } });
-  } catch (err) {
-    res.status(400).json({ status: "Failed", message: "Invalid task id!" });
-  }
-};
-exports.updateTask = async (req, res) => {
-  try {
-    const updatedTask = await taskModel.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    res.status(200).json({ status: "Success", data: { updatedTask } });
-  } catch (err) {
-    res.status(404).json({ status: "Failed", message: err });
-  }
-};
-exports.deleteTask = async (req, res) => {
-  try {
-    const deleteTask = await taskModel.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "Success",
-      message: null,
-    });
-  } catch (err) {
-    res.status(404).json({ status: "Failed", message: err });
-  }
-};
+  // res.status(400).json({ status: "Failed", message: err });
+});
+exports.getTask = catchAsync(async (req, res) => {
+  const user = await taskModel.findById(req.params.id);
+  res.status(200).json({ status: "Success", data: { user } });
+  // res.status(400).json({ status: "Failed", message: "Invalid task id!" });
+});
+exports.updateTask = catchAsync(async (req, res) => {
+  const updatedTask = await taskModel.findByIdAndUpdate(
+    req.params.id,
+    req.body
+  );
+  res.status(200).json({ status: "Success", data: { updatedTask } });
+  // res.status(404).json({ status: "Failed", message: err });
+});
+exports.deleteTask = catchAsync(async (req, res) => {
+  const deleteTask = await taskModel.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "Success",
+    message: null,
+  });
+  // res.status(404).json({ status: "Failed", message: err });
+});
