82781f1afe6a948d38d41aeaa607dd95274e1aba
Tejas Khera <tejaskhera191812@gmail.com>
2025-03-12 23:51:30 +0530

    Added feature to send cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,6 +15,18 @@ const sendToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = sendToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") {
+    cookieOptions.secure = true;
+  }
+  res.cookie("jwt", token, cookieOptions);
+  //Remove password from response
+  user.password = undefined;
   res.status(statusCode).json({ status: "success", token, data: { user } });
 };
 
